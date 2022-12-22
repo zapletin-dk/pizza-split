2ccf5efc01cc61aaad373d2147c404d1b7b23634
Zapletin Daniil <Zaplot1994@gmail.com>
2022-12-22 19:21:11 +0300

    Task done with all test passed

--- src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
+++ src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -1,11 +1,18 @@
 package com.epam.rd.autotasks.pizzasplit;
 
+import java.util.Scanner;
+
 public class PizzaSplit {
     public static void main(String[] args) {
-        //Write a program, reading number of people and number of pieces per pizza and then
-        //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        int people = scanner.nextInt();
+        int pizzaSlices = scanner.nextInt();
+        int newPizza = pizzaSlices;
+        int counter = 1;
+        while (newPizza % people !=0){
+            newPizza += pizzaSlices;
+            counter++;
+        }
+        System.out.println(counter);
     }
 }
